ec1d50c29c439abcfccd5dc1c00fb09239ee215b
leam <leam.murphy1@gmail.com>
2021-06-02 22:22:28 +0000

    copied function from play to client, exported the function to be called in play.js, used object destructuring

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:"135.23.222.131", // IP address here,
+    port: 50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Connected!");
+  });
+
+  return conn;
+};
+
+
+module.exports = {
+ connect
+}
--- play.js
+++ play.js
@@ -1,24 +1,9 @@
-const net = require("net");
+const {connect} = require("./client.js")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:"135.23.222.131", // IP address here,
-    port: 50542 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    console.log("Connected!");
-  });
-
-  // return conn;
-};
-
-
-
-console.log("Connecting ...");
 connect();
+console.log("Connecting...")
+
+
+
+
 
