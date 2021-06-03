0728730e50092e5118bb32687f7a5041293b0fb1
leam <leam.murphy1@gmail.com>
2021-06-03 01:16:41 +0000

    can read user exit input

--- play.js
+++ play.js
@@ -4,6 +4,27 @@ connect();
 console.log("Connecting...")
 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (keyPress) {
+  if (keyPress === '\u0003') {
+    process.exit();
+    // return connection.write("Move Up")
+  }
+  
+};
 
+setupInput();
 
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
